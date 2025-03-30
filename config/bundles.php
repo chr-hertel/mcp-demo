1f1a59147861ff23fb43838f2612b9aba981e32d
Christopher Hertel <mail@christopher-hertel.de>
2025-03-30 22:07:43 +0200

    Add MCP Bundle and Tools

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
+    PhpLlm\McpBundle\McpBundle::class => ['all' => true],
+    PhpLlm\LlmChainBundle\LlmChainBundle::class => ['all' => true],
 ];
